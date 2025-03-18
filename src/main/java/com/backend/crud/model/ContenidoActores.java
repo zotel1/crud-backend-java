6c908fedcd9bd657a7a5555f009e731f67ac2b8f
zotel1 <zotelsigel@gmail.com>
2025-03-17 23:40:04 -0300

    agregamos la tabla de ContenidoActores a nuestro model

---
+++ src/main/java/com/backend/crud/model/ContenidoActores.java
@@ -0,0 +1,47 @@
+package com.backend.crud.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "contenido_actores")
+public class ContenidoActores {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long contenido_actores_id;
+
+    private int contenido_id;
+
+    private int actores_id;
+
+    public ContenidoActores() {}
+
+    public ContenidoActores(int contenido_id, int actores_id) {
+        this.contenido_id = contenido_id;
+        this.actores_id = actores_id;
+    }
+
+    public Long getContenido_actores_id() {
+        return contenido_actores_id;
+    }
+
+    public void setContenido_actores_id(Long contenido_actores_id) {
+        this.contenido_actores_id = contenido_actores_id;
+    }
+
+    public int getContenido_id() {
+        return contenido_id;
+    }
+
+    public void setContenido_id(int contenido_id) {
+        this.contenido_id = contenido_id;
+    }
+
+    public int getActores_id() {
+        return actores_id;
+    }
+
+    public void setActores_id(int actores_id) {
+        this.actores_id = actores_id;
+    }
+}
