a0e55fa4d69becc3ee60e1f6cb8d040761c75c08
zotel1 <zotelsigel@gmail.com>
2025-03-17 22:17:03 -0300

    Initial commit

---
+++ src/main/java/com/backend/crud/CrudRefactorizedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRefactorizedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRefactorizedBackendApplication.class, args);
+	}
+
+}
