7cc6b18f29693416af6f1c4ecfefbda0ba6d2428
zotel1 <zotelsigel@gmail.com>
2025-03-19 23:26:51 -0300

    Agregamos la dependencia de swagger

---
+++ src/main/java/com/backend/crud/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.backend.crud.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/auth")
+@Tag(na)
+public class AuthController {
+}
