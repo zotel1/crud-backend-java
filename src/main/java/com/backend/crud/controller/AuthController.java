9ba39faa8227e1c8c6bc6f102f0a57b48bd84106
zotel1 <zotelsigel@gmail.com>
2025-03-19 23:39:05 -0300

    Agregamos nuestro JwtTokenService

--- src/main/java/com/backend/crud/controller/AuthController.java
+++ src/main/java/com/backend/crud/controller/AuthController.java
@@ -1,12 +1,22 @@
 package com.backend.crud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.authentication.AuthenticationManager;
+
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/auth")
-@Tag()
+@Tag(name = "Autenticación", description = "Endpoints de autenticación")
 public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
 }
---
+++ src/main/java/com/backend/crud/service/JwtTokenService.java
@@ -0,0 +1,11 @@
+package com.backend.crud.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtTokenService {
+
+    private final String apisecret = "TExBVkVfTVVZX1NFQ1JFVEzE3Zmxu7BSGSJx72BSBXM";
+
+    private static final long
+}
