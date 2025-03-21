480198d13c0483a30c60b64cfad18dc032122552
zotel1 <zotelsigel@gmail.com>
2025-03-20 22:38:45 -0300

    Creamos el servicio UserDetailsServiceImpl

--- src/main/java/com/backend/crud/service/JwtTokenService.java
+++ src/main/java/com/backend/crud/service/JwtTokenService.java
@@ -1,5 +1,7 @@
 package com.backend.crud.service;
 
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,5 +9,8 @@ public class JwtTokenService {
 
     private final String apisecret = "TExBVkVfTVVZX1NFQ1JFVEzE3Zmxu7BSGSJx72BSBXM";
 
-    private static final long
+    private static final long ACCESS_TOKEN_VALIDITY = 1000 * 60 * 15; // 15 minutos
+    private static final long REFRESH_TOKEN_VALIDITY = 1000 * 60 * 60 * 24; // 24 horas
+
+    public String generateToken(UserDetails)
 }
---
+++ src/main/java/com/backend/crud/service/UserDetailsServiceImpl.java
@@ -0,0 +1,6 @@
+package com.backend.crud.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+}
