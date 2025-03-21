6ad56cab50430e4fb2bf1edba5187d8616e575a0
zotel1 <zotelsigel@gmail.com>
2025-03-20 23:21:19 -0300

    actualizamos nuestro servicio

--- src/main/java/com/backend/crud/service/UserDetailsServiceImpl.java
+++ src/main/java/com/backend/crud/service/UserDetailsServiceImpl.java
@@ -1,6 +1,33 @@
 package com.backend.crud.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Llamada a la base de datos para obtener el usuario
+        UserModel userModel = this.userRepository.findByUsername(username);
+
+        if (userModel == null) {
+            throw new UsernameNotFoundException("Usuario no encontrado: " + username);
+        }
+
+        System.out.println("Usuario encontrado: " + userModel.getUsername());
+        System.out.println("Rol del usuario: " + userModel.getRole());
+
+        // Normaliza el rol para asegurar que tiene el prefijo "ROLE_"
+        String role = userModel.getRole().startsWith("ROLE_") ? userModel.getRole() : "ROLE_" + userModel.getRole();
+
+        // Retorna un objeto UserDetails con los detalles del usuario
+        return User.builder()
+                .username(userModel.getUsername()) // Nombre de usuario
+                .password(userModel.getPassword()) // Contraseña encriptada
+                .roles(role.replaceFirst("^ROLE_", "")) // Normaliza roles redundantes
+                .build();
+    }
 }
