d5db13a4496594e7e606bd71b86d8ead362da7c7
zotel1 <zotelsigel@gmail.com>
2025-03-22 18:48:05 -0300

    completamos nuestro segundo dto, CategoriaDto, contiene los mismos datos que nuestro model pero sin estar las relaciones entre las tablas

--- src/main/java/com/backend/crud/dto/CategoriaDto.java
+++ src/main/java/com/backend/crud/dto/CategoriaDto.java
@@ -1,4 +1,29 @@
 package com.backend.crud.dto;
 
 public class CategoriaDto {
+
+    private Long categoria_id;
+    private String nombre;
+
+    public CategoriaDto() {}
+
+    public CategoriaDto(String nombre){
+        this.nombre = nombre;
+    }
+
+    public Long getCategoria_id() {
+        return categoria_id;
+    }
+
+    public void setCategoria_id(Long categoria_id) {
+        this.categoria_id = categoria_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
