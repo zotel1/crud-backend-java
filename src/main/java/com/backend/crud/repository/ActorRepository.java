0947425ba100d910a5a313f5ff0c2ffc9358cddf
zotel1 <zotelsigel@gmail.com>
2025-03-22 19:17:25 -0300

    actualizamos nuestro ActorRepository

--- src/main/java/com/backend/crud/repository/ActorRepository.java
+++ src/main/java/com/backend/crud/repository/ActorRepository.java
@@ -1,4 +1,12 @@
 package com.backend.crud.repository;
 
-public interface ActorRepository {
+import com.backend.crud.model.ActorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ActorRepository extends JpaRepository<ActorModel, Long> {
+    List<ActorModel> findByNombreContainingIgnoreCase(String nombre);
 }
