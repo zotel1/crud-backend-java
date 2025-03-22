497cd1a3114fbf2596a33ce9b7c8744728c9e917
zotel1 <zotelsigel@gmail.com>
2025-03-22 19:11:43 -0300

    completamos nuestro cuarto dto, GeneroDto, contiene los mismos datos que nuestro model pero sin estar las relaciones entre las tablas

--- src/main/java/com/backend/crud/dto/GeneroDto.java
+++ src/main/java/com/backend/crud/dto/GeneroDto.java
@@ -1,4 +1,45 @@
 package com.backend.crud.dto;
 
+import com.backend.crud.model.ContenidoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GeneroDto {
+
+    private Long id;
+    private String nombre;
+
+    private List<ContenidoModel> contenidos = new ArrayList<>();
+
+    public GeneroDto() {};
+
+    public GeneroDto(String nombre, List<ContenidoModel> contenidos){
+        this.nombre = nombre;
+        this.contenidos = contenidos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<ContenidoModel> getContenidos() {
+        return contenidos;
+    }
+
+    public void setContenidos(List<ContenidoModel> contenidos) {
+        this.contenidos = contenidos;
+    }
 }
