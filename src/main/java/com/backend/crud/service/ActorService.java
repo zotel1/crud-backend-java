b3a1f66211567063f608069d68fefefbfbfaa88b
zotel1 <zotelsigel@gmail.com>
2025-03-22 19:19:55 -0300

    actualizamos nuestro ActorService

--- src/main/java/com/backend/crud/service/ActorService.java
+++ src/main/java/com/backend/crud/service/ActorService.java
@@ -1,15 +1,24 @@
 package com.backend.crud.service;
 
+import com.backend.crud.model.ActorModel;
+import com.backend.crud.repository.ActorRepository;
 import com.backend.crud.repository.CategoriaRepository;
 import com.backend.crud.repository.ContenidoRepository;
 import com.backend.crud.repository.GeneroRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ActorService {
 
-    private final CategoriaRepository categoriaRepository;
-    private final ContenidoRepository contenidoRepository;
-    private final GeneroRepository generoRepository;
+    private final ActorRepository actorRepository;
 
+    public ActorService(ActorRepository actorRepository) {
+        this.actorRepository = actorRepository;
+    }
+
+    public List<ActorModel> obtenerTodos(){
+        return actorRepository.findAll();
+    }
 }
