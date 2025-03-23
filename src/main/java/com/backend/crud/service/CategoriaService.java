785ea08c40168594446746dbf75c35f63b415eb1
zotel1 <zotelsigel@gmail.com>
2025-03-23 20:20:21 -0300

    Actualizamos nuestro CategoriaService agregando un metodo "obtenerCategorias" para traer todas las categorias

--- src/main/java/com/backend/crud/service/CategoriaService.java
+++ src/main/java/com/backend/crud/service/CategoriaService.java
@@ -1,4 +1,22 @@
 package com.backend.crud.service;
 
+import com.backend.crud.model.CategoriaModel;
+import com.backend.crud.repository.ActorRepository;
+import com.backend.crud.repository.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoriaService {
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public List<CategoriaModel> obtenercategorias(){
+        return categoriaRepository.findAll();
+    }
+
 }
