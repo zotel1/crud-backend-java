4dca72d090b7be5b3fd4e79216afda8198478458
zotel1 <zotelsigel@gmail.com>
2025-03-23 19:14:50 -0300

    Actualizamos nuestro controller ActorController para obtener actores por nombre con el metodo buscarActoresnombre

--- src/main/java/com/backend/crud/controller/ActorController.java
+++ src/main/java/com/backend/crud/controller/ActorController.java
@@ -4,10 +4,7 @@ import com.backend.crud.dto.ActorDto;
 import com.backend.crud.model.ActorModel;
 import com.backend.crud.service.ActorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,4 +29,18 @@ public class ActorController {
         return ResponseEntity.ok(actores);
     }
 
+    // Buscamos actores por nombre
+    @GetMapping("/{nombre}")
+    public ResponseEntity<?> buscarActorNombre(@PathVariable String nombre) {
+        if (nombre.trim().isEmpty()) {
+            return ResponseEntity.badRequest().body("El nombre debe tener al menos 1 carácter.");
+        }
+
+        List<ActorModel> actores = actorService.buscarActorNombre(nombre);
+        if (actores.isEmpty()) {
+            return ResponseEntity.status(404).body("No hay actores registrados.");
+        }
+
+        return ResponseEntity.ok(actores);
+    }
 }
