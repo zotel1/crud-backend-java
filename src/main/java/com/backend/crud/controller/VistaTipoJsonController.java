258acaf0f17e38ce305380705ab4bf97009f0a20
zotel1 <zotelsigel@gmail.com>
2025-03-25 23:14:31 -0300

    creamos nuestro controlador para la vista VistaTipoJsonController

---
+++ src/main/java/com/backend/crud/controller/VistaTipoJsonController.java
@@ -0,0 +1,9 @@
+package com.backend.crud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalogo")
+public class VistaTipoJsonController {
+}
