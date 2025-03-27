39c5f86c35e17bc737d08a3cf01e3d2f961c6cbb
zotel1 <zotelsigel@gmail.com>
2025-03-26 23:37:13 -0300

    en nuestro VistaTipoJsonController creamos nuestro primer metodo get para obtenerCategorias

--- src/main/java/com/backend/crud/controller/VistaTipoJsonController.java
+++ src/main/java/com/backend/crud/controller/VistaTipoJsonController.java
@@ -1,9 +1,30 @@
 package com.backend.crud.controller;
 
+import com.backend.crud.model.ContenidoModel;
+import com.backend.crud.service.VistaTipoJsonService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/catalogo")
 public class VistaTipoJsonController {
+
+    private final VistaTipoJsonService vistaTipoJsonService;
+
+    public VistaTipoJsonController(VistaTipoJsonService vistaTipoJsonService) {
+        this.vistaTipoJsonService = vistaTipoJsonService;
+    }
+
+    @GetMapping("/categoria")
+    public ResponseEntity<?> obtenerCategoria() {
+        List<ContenidoModel> categorias = vistaTipoJsonService.obtenerCategorias();
+        if (categorias.isEmpty()) {
+            return ResponseEntity.status(400).body("No hay categorías disponibles.");
+        }
+        return ResponseEntity.ok(categorias);
+    }
 }
