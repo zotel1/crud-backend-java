700c84fa44061a7cdc59cef24f151918a1ce69de
zotel1 <zotelsigel@gmail.com>
2025-03-27 23:08:59 -0300

    Agregamos el metodo para buscar por generos, pero debo revisar en que model lo tengo presente

--- src/main/java/com/backend/crud/controller/VistaTipoJsonController.java
+++ src/main/java/com/backend/crud/controller/VistaTipoJsonController.java
@@ -5,6 +5,7 @@ import com.backend.crud.model.ContenidoModel;
 import com.backend.crud.service.VistaTipoJsonService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,4 +35,26 @@ public class VistaTipoJsonController {
         List<ContenidoModel> catalogo = vistaTipoJsonService.obtenerCatalogo();
         return ResponseEntity.ok(catalogo);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+        List<ContenidoModel> resultado = vistaTipoJsonService.buscarPorId(id);
+        if (resultado.isEmpty()) {
+            return ResponseEntity.status(404).body("No se encontraron resultados para este ID.");
+        }
+
+        return ResponseEntity.ok(resultado);
+    }
+
+    @GetMapping("/genero/{genero}")
+    public ResponseEntity<?> buscarPorGenero(@PathVariable String genero) {
+        List<ContenidoModel> resultado = vistaTipoJsonService.buscarPorId(genero);
+        if (resultado.isEmpty()) {
+            return ResponseEntity.status(404).body("No se encontraron resultados para este género.");
+        }
+
+        return ResponseEntity.ok(resultado);
+    }
+
+
 }
